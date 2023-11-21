a0ab6104ffff7263a53a13ffd9f8afba5824cac9
Tobitastik <144216903+Tobitastik@users.noreply.github.com>
2023-11-21 10:00:07 +0100

    FileIO færdig, fejl på 100bedsteserier.txt, så uploader en ny for en sikkerheds skyld

--- src/FileIO.java
+++ src/FileIO.java
@@ -6,36 +6,37 @@ import java.util.Scanner;
 public class FileIO {
 
     public ArrayList<String[]> readFilmData(String path) {
-        ArrayList<String[]> data = new ArrayList<>();
-        File file = new File(path);
+        ArrayList<String[]> dataF = new ArrayList<>();
+        File fileF = new File(path);
 
-        try (Scanner scanner = new Scanner(file)) {
+        try (Scanner scanner = new Scanner(fileF)) {
             while (scanner.hasNextLine()) {
                 String line = scanner.nextLine();
-                String[] tempDataF = line.split(",");
-                data.add(tempDataF);
+                String[] tempDataF = line.split(";");
+                dataF.add(tempDataF);
 
             }
         } catch (FileNotFoundException e) {
             System.out.println("File not found (Film)");
         }
 
-        return data;
+        return dataF;
     }
 
-   public ArrayList<String[]> readSerieData(String path) {
+    public ArrayList<String[]> readSerieData(String path) {
         ArrayList<String[]> dataS = new ArrayList<>();
-        File file2 = new File(path);
+        File fileS = new File(path);
 
-        try (Scanner scanner = new Scanner(file2)) {
+        try (Scanner scanner = new Scanner(fileS)) {
             while (scanner.hasNextLine()) {
                 String line = scanner.nextLine();
-                String[] tempDataS = line.split(",");
+                String[] tempDataS = line.split(";");
                 dataS.add(tempDataS);
             }
         } catch (FileNotFoundException e) {
             System.out.println("File not found (Serie)");
         }
+
         return dataS;
-   }
+    }
 }
